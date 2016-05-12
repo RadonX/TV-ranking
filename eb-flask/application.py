c21419df8a16c27c1431b2bf8603095315386922
RadonX <radon93@hotmail.com>
2016-05-12 12:10:46 -0400

    more templates

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, jsonify, render_template, Response #request
+from flask import Flask, render_template, Response #request, jsonify
 from flask import url_for
 import sys
 import requests
@@ -14,13 +14,25 @@ import logging
 
 application = Flask(__name__, static_url_path='/static')
 
-@application.route('/')
-def index():
+@application.route('/ranking')
+def ranking():
     r = requests.get('http://localhost:5001/static/get_ranked.json')
     # start another flask server (in `/data-flask`) to host the json
     # since in the final demo, I need to request data from another domain (provided by John)
     json_response = r.json()
-    return render_template('main.html', TV_list = json_response)
+    return render_template('ranking.html', TV_list = json_response)
+
+@application.route('/')
+def index():
+    return render_template('index.html')
+
+@application.route('/tv')
+def tv():
+    return render_template('tv.html')
+
+@application.route('/search')
+def search():
+    return render_template('search.html')
 
 @application.route('/get_ranked')
 def getDetails():
